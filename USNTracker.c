89351c84ac6be015052e5c0d0b991f0f729a5fad
Ninjapenguin16 <rylanmatthewdean@gmail.com>
2024-09-06 12:38:14 -0500

    Add admin check

--- USNTracker.c
+++ USNTracker.c
@@ -44,64 +44,46 @@ BOOL GetFullPathByFileReference(HANDLE hVol, DWORDLONG fileReferenceNumber, wcha
 void PrintEventReason(DWORD reason) {
 
     // This seems stupid and it is but not much of a better way to do it
-    if (reason & USN_REASON_FILE_CREATE) {
+    if(reason & USN_REASON_FILE_CREATE)
         wprintf(L"  - File created\n");
-    }
-    if (reason & USN_REASON_FILE_DELETE) {
+    if(reason & USN_REASON_FILE_DELETE)
         wprintf(L"  - File deleted\n");
-    }
-    if (reason & USN_REASON_DATA_OVERWRITE) {
+    if(reason & USN_REASON_DATA_OVERWRITE)
         wprintf(L"  - Data overwritten\n");
-    }
-    if (reason & USN_REASON_DATA_EXTEND) {
+    if(reason & USN_REASON_DATA_EXTEND)
         wprintf(L"  - Data extended\n");
-    }
-    if (reason & USN_REASON_DATA_TRUNCATION) {
+    if(reason & USN_REASON_DATA_TRUNCATION)
         wprintf(L"  - Data truncated\n");
-    }
-    if (reason & USN_REASON_RENAME_NEW_NAME) {
+    if(reason & USN_REASON_RENAME_NEW_NAME)
         wprintf(L"  - Renamed (new name)\n");
-    }
-    if (reason & USN_REASON_RENAME_OLD_NAME) {
+    if(reason & USN_REASON_RENAME_OLD_NAME)
         wprintf(L"  - Renamed (old name)\n");
-    }
-    if (reason & USN_REASON_CLOSE) {
+    if(reason & USN_REASON_CLOSE)
         wprintf(L"  - File closed\n");
-    }
-    if (reason & USN_REASON_SECURITY_CHANGE) {
+    if(reason & USN_REASON_SECURITY_CHANGE)
         wprintf(L"  - Security changed\n");
-    }
-    if (reason & USN_REASON_BASIC_INFO_CHANGE) {
+    if(reason & USN_REASON_BASIC_INFO_CHANGE)
         wprintf(L"  - Basic info changed\n");
-    }
-    if (reason & USN_REASON_EA_CHANGE) {
+    if(reason & USN_REASON_EA_CHANGE)
         wprintf(L"  - Extended attributes changed\n");
-    }
-    if (reason & USN_REASON_INDEXABLE_CHANGE) {
+    if(reason & USN_REASON_INDEXABLE_CHANGE)
         wprintf(L"  - Indexing status changed\n");
-    }
-    if (reason & USN_REASON_HARD_LINK_CHANGE) {
+    if(reason & USN_REASON_HARD_LINK_CHANGE)
         wprintf(L"  - Hard link changed\n");
-    }
-    if (reason & USN_REASON_COMPRESSION_CHANGE) {
+    if(reason & USN_REASON_COMPRESSION_CHANGE)
         wprintf(L"  - Compression changed\n");
-    }
-    if (reason & USN_REASON_ENCRYPTION_CHANGE) {
+    if(reason & USN_REASON_ENCRYPTION_CHANGE)
         wprintf(L"  - Encryption changed\n");
-    }
-    if (reason & USN_REASON_OBJECT_ID_CHANGE) {
+    if(reason & USN_REASON_OBJECT_ID_CHANGE)
         wprintf(L"  - Object ID changed\n");
-    }
-    if (reason & USN_REASON_REPARSE_POINT_CHANGE) {
+    if(reason & USN_REASON_REPARSE_POINT_CHANGE)
         wprintf(L"  - Reparse point changed\n");
-    }
-    if (reason & USN_REASON_STREAM_CHANGE) {
+    if(reason & USN_REASON_STREAM_CHANGE)
         wprintf(L"  - Stream changed\n");
-    }
 }
 
 // Function to print file event message with full path and details
-void PrintFileEventMessage(LPCWSTR fullPath, DWORD reason, USN Usn) {
+void PrintFileEventMessage(LPCWSTR fullPath, DWORD reason, USN Usn){
 
     printf("%lld- ", Usn);
 
@@ -109,7 +91,65 @@ void PrintFileEventMessage(LPCWSTR fullPath, DWORD reason, USN Usn) {
     PrintEventReason(reason);
 }
 
+int IsRunningAsAdmin(){
+
+    BOOL isAdmin = FALSE;
+    HANDLE hToken = NULL;
+
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+    {
+        TOKEN_ELEVATION elevation;
+        DWORD dwSize;
+
+        if (GetTokenInformation(hToken, TokenElevation, &elevation, sizeof(elevation), &dwSize))
+        {
+            isAdmin = elevation.TokenIsElevated;
+        }
+        CloseHandle(hToken);
+    }
+    return isAdmin;
+}
+
+int RestartAsAdmin(int argc, wchar_t* argv[]){
+
+    // Build the argument list to pass to the elevated instance
+    wchar_t commandLine[1024] = { 0 };
+
+    for(int i = 1; i < argc; i++){
+        wcscat(commandLine, L"\"");
+        wcscat(commandLine, argv[i]);
+        wcscat(commandLine, L"\" ");
+    }
+
+    // Relaunch the program with admin rights
+    SHELLEXECUTEINFOW sei = { sizeof(sei) };
+    sei.lpVerb = L"runas";  // Request elevation
+    sei.lpFile = argv[0];   // Path to the current executable
+    sei.lpParameters = commandLine;  // Pass arguments
+    sei.hwnd = NULL;
+    sei.nShow = SW_NORMAL;
+
+    if(!ShellExecuteExW(&sei)){
+
+        DWORD dwError = GetLastError();
+        if (dwError == ERROR_CANCELLED)
+            MessageBox(NULL, L"This program must be run as admin to work", L"Permissions Error", MB_OK | MB_ICONERROR);
+        else
+            MessageBox(NULL, L"Failed to elevate despite user allowance", L"Permissions Error", MB_OK | MB_ICONERROR);
+
+        return 1;
+    }
+
+    return 0; // Elevated process will restart
+}
+
 int wmain(int argc, wchar_t* argv[]) {
+
+    // Make sure program is run as admin
+    // and prompt to run as admin if not
+    if(!IsRunningAsAdmin())
+        return RestartAsAdmin(argc, argv);
+
     HANDLE hVol = INVALID_HANDLE_VALUE;
     DWORD bytesReturned = 0;
     BYTE buffer[BUFFER_SIZE] = { 0 };
@@ -123,24 +163,24 @@ int wmain(int argc, wchar_t* argv[]) {
         targetFolder = argv[1];
     }
 
-    // Step 1: Open the volume (C drive)
+    // Open file pointer to the C drive
     hVol = CreateFileW(L"\\\\.\\C:", GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
     if (hVol == INVALID_HANDLE_VALUE) {
         printf("Failed to open C drive.\n");
         return 1;
     }
 
-    // Step 2: Query the USN Journal to get its properties
+    // Get the USN Journal's properties
     if (!DeviceIoControl(hVol, FSCTL_QUERY_USN_JOURNAL, NULL, 0, &usnJournalData, sizeof(usnJournalData), &bytesReturned, NULL)) {
         printf("Failed to query USN journal.\n");
         CloseHandle(hVol);
         return 1;
     }
 
-    // Initialize the starting USN value
+    // Initialize USN value
     lastUsn = usnJournalData.NextUsn;
 
-    // Step 3: Monitor the USN Journal for changes
+    // Setup USN Journal monitoring
     READ_USN_JOURNAL_DATA readData = { 0 };
     readData.ReasonMask = USN_REASON_FILE_CREATE | USN_REASON_FILE_DELETE | USN_REASON_DATA_OVERWRITE | USN_REASON_DATA_EXTEND |
                           USN_REASON_DATA_TRUNCATION | USN_REASON_RENAME_NEW_NAME | USN_REASON_RENAME_OLD_NAME |
